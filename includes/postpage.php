89712665ca52ec242753d55307c62d3d37291740
Andrew R McHugh <arm5629@gmail.com>
2015-06-12 19:17:56 -0400

    Seeding the pre and post pages

--- includes/postpage.php
+++ includes/postpage.php
@@ -1,1 +1,6 @@
-postpage.php
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="dist/js/iamaprototype.min.js"></script>
+   </body>
+</html>
