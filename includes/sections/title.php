de526e35413f6f9ca733783a28cea2b1f3c442e1
Andrew R McHugh <arm5629@gmail.com>
2015-06-18 18:41:58 -0400

    added background img to title.php

--- includes/sections/title.php
+++ includes/sections/title.php
@@ -1,4 +1,4 @@
-<section class="sec-title">
+<section class="sec-title" style="background-image: url('/img/girl-back.jpg')">
    <div class="row">
       <div class="col-xs-12">
          <h2>Be a</h2>
