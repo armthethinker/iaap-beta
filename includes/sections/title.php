509137d15acce776e48abcb42d8b332fc9a7ddd0
Andrew R McHugh <arm5629@gmail.com>
2015-06-18 19:13:45 -0400

    Added section overlay

--- includes/sections/title.php
+++ includes/sections/title.php
@@ -1,8 +1,10 @@
 <section class="sec-title" style="background-image: url('/img/girl-back.jpg')">
-   <div class="row">
-      <div class="col-xs-12">
-         <h2>Be a</h2>
-         <h1>PROTOTYPE</h1>
+   <div class="section-overlay">
+      <div class="row">
+         <div class="col-xs-12">
+            <h2>Be a</h2>
+            <h1>PROTOTYPE</h1>
+         </div>
       </div>
    </div>
 </section>
