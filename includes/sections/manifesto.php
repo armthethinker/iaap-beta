77e7992a11f730c3cb425310ad81981e32ab8bea
Andrew R McHugh <arm5629@gmail.com>
2015-06-18 19:58:34 -0400

    Added manifesto container

--- includes/sections/manifesto.php
+++ includes/sections/manifesto.php
@@ -1,8 +1,17 @@
 <section class="sec-manifesto" style="background-image: url('/img/artist-blur.jpg')">
    <div class="section-overlay">
       <div class="row">
-         <div class="col-xs-12">
-            <h1>manifesto</h1>
+         <div class="col-lg-8 col-lg-offset-2 col-sm-10 col-sm-offset-1" >
+            <div class="manifesto-container">
+               <div class="pull-right">
+                  <span class="badge">v1</span>
+               </div>
+               <h3>
+                  Manifesto
+                  <div class="underline" domFlag></div>
+               </h3>
+               <?php include('includes/md/MANIFESTO.html') ?>
+            </div>
          </div>
       </div>
    </div>
