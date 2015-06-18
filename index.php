da5af44e164360e231b62ee5472bd106444b2c2a
Andrew R McHugh <arm5629@gmail.com>
2015-06-18 18:13:36 -0400

    setup main sections

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('includes/prepage.php') ?>
+
+<?php include('includes/sections/title.php') ?>
+<?php include('includes/sections/manifesto.php') ?>
+<?php include('includes/sections/we-are.php') ?>
+<?php include('includes/sections/founders.php') ?>
+<?php include('includes/sections/action.php') ?>
+<?php include('includes/sections/faqs.php') ?>
+<?php include('includes/sections/footer.php') ?>
+
+<?php include('includes/postpage.php') ?>
