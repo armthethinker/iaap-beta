f72edd356a9f4dbbaff2292f45db90bc686c8ef2
Andrew R McHugh <arm5629@gmail.com>
2015-06-19 09:04:15 -0400

    Adding real fonts

--- includes/prepage.php
+++ includes/prepage.php
@@ -9,7 +9,8 @@
 
    <!-- Stylez -->
    <link href="dist/css/iamaprototype.css" rel="stylesheet">
-
+   <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,400italic|Montserrat' rel='stylesheet' type='text/css'>
+   
    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
    <!--[if lt IE 9]>
