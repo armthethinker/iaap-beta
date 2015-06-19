0a1205da1911c5d5f855fd9eb0959af502ca10fd
Andrew R McHugh <arm5629@gmail.com>
2015-06-19 19:52:00 -0400

    Changed "founders" to "story"

--- includes/sections/founders.php
+++
@@ -1,9 +0,0 @@
-<section class="sec-founders" style="background-image: url('/img/founders.jpg')">
-   <div class="section-overlay">
-      <div class="row">
-         <div class="col-xs-12">
-            <h1>founders</h1>
-         </div>
-      </div>
-   </div>
-</section>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php include('includes/sections/title.php') ?>
 <?php include('includes/sections/manifesto.php') ?>
 <?php include('includes/sections/we-are.php') ?>
-<?php include('includes/sections/founders.php') ?>
+<?php include('includes/sections/story.php') ?>
 <?php include('includes/sections/action.php') ?>
 <?php include('includes/sections/faqs.php') ?>
 <?php include('includes/sections/footer.php') ?>
