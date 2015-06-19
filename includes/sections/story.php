af01d37aaf4955d3510b628805fdf5f88faa6f46
Andrew R McHugh <arm5629@gmail.com>
2015-06-19 19:52:09 -0400

    Added filler content and styles

---
+++ includes/sections/story.php
@@ -0,0 +1,51 @@
+<section class="sec-story">
+   <div class="section-overlay">
+      <div class="row">
+         <div class="col-xs-12">
+            <h1>Our Story</h1>
+            <h2><small>Three Living Prototypes</small></h2>
+         </div>
+         <div class="col-lg-6">
+            <p class="fixie"></p>
+            <p class="fixie"></p>
+            <p class="fixie"></p>
+            <p class="fixie"></p>
+         </div>
+         <div class="col-lg-6">
+            <div class="media profile-block">
+               <div class="media-left">
+                  <a href="http://twitter.com/armthethinker">
+                     <img src="img/prof-andrew.png">
+                  </a>
+               </div>
+               <div class="media-body">
+                  <h4 class="media-title">Andrew R McHugh</h4>
+                  <p class="fixie">I did this. I did that. Now I do the do.</p>
+               </div>
+            </div>
+            <div class="media profile-block">
+               <div class="media-left">
+                  <a href="http://twitter.com/armthethinker">
+                     <img src="img/prof-sarah.png">
+                  </a>
+               </div>
+               <div class="media-body">
+                  <h4 class="media-title">Sarah Kurland</h4>
+                  <p class="fixie">I did this. I did that. Now I do the do.</p>
+               </div>
+            </div>
+            <div class="media profile-block">
+               <div class="media-left">
+                  <a href="http://twitter.com/armthethinker">
+                     <img src="img/prof-abby.png">
+                  </a>
+               </div>
+               <div class="media-body">
+                  <h4 class="media-title">Abby Stokes</h4>
+                  <p class="fixie">I did this. I did that. Now I do the do.</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
