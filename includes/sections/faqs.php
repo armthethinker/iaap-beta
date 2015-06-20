bc579ece8d3e37761e970200d62e13d60cb48bc3
Andrew R McHugh <arm5629@gmail.com>
2015-06-19 20:28:50 -0400

    Added faqs section

--- includes/sections/faqs.php
+++ includes/sections/faqs.php
@@ -2,7 +2,40 @@
    <div class="section-overlay">
       <div class="row">
          <div class="col-xs-12">
-            <h1>faqs</h1>
+            <button class="pull-right btn btn-default btn-faqs" data-toggle="collapse" data-target=".faqs">Show</button>
+            <h1>FAQs</h1>
+            <div class="faqs collapse">
+               <div class="row">
+                  <div class="col-md-6">
+                     <div class="faq">
+                        <h4>Question what?</h4>
+                        <p class="fixie"></p>
+                     </div>
+                     <div class="faq">
+                        <h4>Question what?</h4>
+                        <p class="fixie"></p>
+                     </div>
+                     <div class="faq">
+                        <h4>Question what?</h4>
+                        <p class="fixie"></p>
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="faq">
+                        <h4>Question what?</h4>
+                        <p class="fixie"></p>
+                     </div>
+                     <div class="faq">
+                        <h4>Question what?</h4>
+                        <p class="fixie"></p>
+                     </div>
+                     <div class="faq">
+                        <h4>Question what?</h4>
+                        <p class="fixie"></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
       </div>
    </div>
