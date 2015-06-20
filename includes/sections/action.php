4c178138b5165f1cafee8501fed59831c039c367
Andrew R McHugh <arm5629@gmail.com>
2015-06-19 20:15:16 -0400

    Added content

--- includes/sections/action.php
+++ includes/sections/action.php
@@ -2,7 +2,39 @@
    <div class="section-overlay">
       <div class="row">
          <div class="col-xs-12">
-            <h1>action</h1>
+            <div class="row">
+               <div class="col-md-8">
+                  <h1>Take Action</h1>
+                  <p class="fixie"></p>
+                  <p>this goes nowhere <a href="#nowhere">nowherelink</a>.</p>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="media media-action">
+                     <div class="media-left">
+                        <div class="media-bullet">1</div>
+                     </div>
+                     <div class="media-body">
+                        <h4>Tell Your Story</h4>
+                        <p>Tweet your proto-story in 145 characters + #IAmAPrototype.</p>
+                        <button class="btn btn-default">Tweet Story</button>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <div class="media media-action">
+                     <div class="media-left">
+                        <div class="media-bullet">2</div>
+                     </div>
+                     <div class="media-body">
+                        <h4>Buy A Thing</h4>
+                        <p>Show your love, spread the word, and help fello prototypes out.</p>
+                        <button class="btn btn-default">Shop Things</button>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
       </div>
    </div>
