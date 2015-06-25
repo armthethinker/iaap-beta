48024b207c5b5740fe9d7f2437243e89dcb41395
Andrew R McHugh <arm5629@gmail.com>
2015-06-24 20:44:47 -0400

    Typo and testing tweets and showcased photos

--- includes/sections/action.php
+++ includes/sections/action.php
@@ -19,6 +19,25 @@
                         <h4>Tell Your Story</h4>
                         <p>Tweet your proto-story in 145 characters + #IAmAPrototype.</p>
                         <button class="btn btn-default">Tweet Story</button>
+                        <hr>
+                        <div class="media tweet">
+                           <div class="media-left">
+                              <i class="fa fa-twitter text-primary"></i>
+                           </div>
+                           <div class="media-body">
+                              <span class="tweet-body">Dot in the sky. Dead pixel. #iamaprototype</span><br>
+                              <span class="tweet-author">- <a href="http://twitter.com" target="_blank">@giantmonkey2</a></span>
+                           </div>
+                        </div>
+                        <div class="media tweet">
+                           <div class="media-left">
+                              <i class="fa fa-twitter text-primary"></i>
+                           </div>
+                           <div class="media-body">
+                              <span class="tweet-body">I win lottery. Sun goes nova. #iamaprototype</span><br>
+                              <span class="tweet-author">- <a href="http://twitter.com" target="_blank">@StevenMeretzky</a></span>
+                           </div>
+                        </div>
                      </div>
                   </div>
                </div>
@@ -29,8 +48,21 @@
                      </div>
                      <div class="media-body">
                         <h4>Buy A Thing</h4>
-                        <p>Show your love, spread the word, and help fello prototypes out.</p>
+                        <p>Show your love, spread the word, and help fellow prototypes out.</p>
                         <button class="btn btn-default">Shop Things</button>
+                        <hr>
+                        <div class="row">
+                           <div class="col-md-6">
+                              <a href="#">
+                                 <div class="img-showcase" style="background-image: url('/Lightroom/IAAP-4.jpg')"></div>
+                              </a>
+                           </div>
+                           <div class="col-md-6">
+                              <a href="#">
+                                 <div class="img-showcase" style="background-image: url('/Lightroom/IAAP-11.jpg')"></div>
+                              </a>
+                           </div>
+                        </div>
                      </div>
                   </div>
                </div>
