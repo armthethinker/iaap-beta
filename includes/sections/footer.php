1a414131b0bba1cfdd3f0843eb631637107182e8
Andrew R McHugh <arm5629@gmail.com>
2015-06-24 22:20:36 -0400

    Added content

--- includes/sections/footer.php
+++ includes/sections/footer.php
@@ -1,8 +1,23 @@
 <section class="sec-footer" style="background-image: url('/img/footer.jpg')">
    <div class="section-overlay">
       <div class="row">
-         <div class="col-xs-12">
-            <h1>footer</h1>
+         <div class="col-md-8">
+            <h5\6\5>Sign up for snippets of awesome, not spam</h5\6\5>
+            <div class="form-group">
+               <div class="input-group">
+                  <input type="email" class="form-control" placeholder="Email address">
+                  <span class="input-group-btn">
+                     <button class="btn btn-primary" type="button">Sign Up</button>
+                  </span>
+               </div>
+            </div>
+            <p class="small">You’ll be the first to know about our stories, new products, and updates as the movement grows. Plus, we’ll give you 5% off of a purchase!</p>
+         </div>
+         <div class="col-md-4 text-right">
+            <a href="#">Store</a><br>
+            <a href="#">Press Kit</a><br>
+            <a href="#"><i class="fa fa-facebook-official"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
          </div>
       </div>
    </div>
