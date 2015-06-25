892c9f60f0764739bfff62b08a4bae35657a86e2
Andrew R McHugh <arm5629@gmail.com>
2015-06-24 21:47:07 -0400

    Tinkering with background highlight and a:hover

--- includes/sections/faqs.php
+++ includes/sections/faqs.php
@@ -4,34 +4,36 @@
          <div class="col-xs-12">
             <button class="pull-right btn btn-default btn-faqs" data-toggle="collapse" data-target=".faqs">Show</button>
             <h1>FAQs</h1>
-            <div class="faqs collapse">
+            <!-- <div class="faqs collapse"> -->
+            <div class="faqs">
                <div class="row">
                   <div class="col-md-6">
                      <div class="faq">
-                        <h4>Question what?</h4>
-                        <p class="fixie"></p>
+                        <h4>What is a "prototype"?</h4>
+                        <p>A prototype is an early version of a final product used to test the direction of a product. As good humans, we’re always prototypes, always testing our limits, always trying a new direction.</p>
                      </div>
                      <div class="faq">
-                        <h4>Question what?</h4>
-                        <p class="fixie"></p>
+                        <h4>What is your next direction?</h4>
+                        <p>We’re a prototype in every which way, including our company. We’ll always be testing new things. Right now, we’re focused on launching and connecting this community. Soon, we’ll show you a few things we have up our sleeves.</p>
+                        <p>Have an idea? Shoot us an <a href="#">email</a>.</p>
                      </div>
                      <div class="faq">
-                        <h4>Question what?</h4>
-                        <p class="fixie"></p>
+                        <h4>What about stories longer than a tweet?</h4>
+                        <p>We love those too! We’re working on a Medium publication for just that. If you want to submit a story, or would like one of us to interview you and write your story, shoot us an <a href="#">email</a>.</p>
                      </div>
                   </div>
                   <div class="col-md-6">
                      <div class="faq">
-                        <h4>Question what?</h4>
-                        <p class="fixie"></p>
+                      <h4>Where does my money go?</h4>
+                        <p>Every purchase directly affects a fellow prototype. A good chunk of your purchase goes towards printing and shipping your item. The profit is split between the founders and 15% of that profit goes into our <a href="#">Prototype Fund</a>.</p>
                      </div>
                      <div class="faq">
-                        <h4>Question what?</h4>
-                        <p class="fixie"></p>
+                        <h4>Prototype Fund?</h4>
+                        <p>When we started this, we knew that we wanted to give back. Each of us have struggled to fund our ideas, or even just to keep the lights on. That’s why 15% of each purchase is put into a revolving fund. In disbursments of $1,000, we’ll give prototypes funding and a bit of mentorship. Applications will open once we’ve built up enough of a fund.</p>
                      </div>
                      <div class="faq">
-                        <h4>Question what?</h4>
-                        <p class="fixie"></p>
+                        <h4>I got charged multiple times for shipping.</h4>
+                        <p>That isn't a question, but we understand what you mean. Because of how our operations are curently setup, we don’t make your order until you buy it. The upside is our products are always available. The downside is that sometimes you get charged twice when purchasing different items together.</p>
                      </div>
                   </div>
                </div>
