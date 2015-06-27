a9e6c02b4412a7ed765c16e07bca2392dc9f6b1c
Andrew R McHugh <arm5629@gmail.com>
2015-06-27 10:35:35 -0400

    Title update

--- includes/prepage.php
+++ includes/prepage.php
@@ -5,7 +5,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-   <title>IAMAPROTOTYPE - Being Change</title>
+   <title>I Am A Prototype - Being Change</title>
 
    <!-- Stylez -->
    <link href="dist/css/iamaprototype.css" rel="stylesheet">
