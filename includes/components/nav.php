82f01bb7e56b4d1469aafc3bdc354f94aff0ae3e
Andrew R McHugh <arm5629@gmail.com>
2015-06-27 09:54:10 -0400

    Moved navigation and restyled

---
+++ includes/components/nav.php
@@ -0,0 +1,23 @@
+<nav class="navbar">
+   <div class="row">
+      <div class="col-xs-2">
+         <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+               <i class="fa fa-bars"></i>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+               <li><a href="#sec-title">Hero</a></li>
+               <li><a href="#sec-manifesto">Manifesto</a></li>
+               <li><a href="#sec-we-are">We Are</a></li>
+               <li><a href="#sec-story">Our Story</a></li>
+               <li><a href="#sec-action">Take Action</a></li>
+               <li><a href="#sec-faqs">FAQs</a></li>
+               <li><a href="#sec-footer">Get Some Newsletter</a></li>
+            </ul>
+         </div>
+      </div>
+      <div class="col-xs-8 text-center">
+         <img src="img/logo.png" height="75px">
+      </div>
+   </div>
+</nav>
--- includes/sections/title.php
+++ includes/sections/title.php
@@ -1,25 +1,6 @@
 <section class="sec-title" style="background-image: url('/img/girl-back.jpg')">
    <div class="section-overlay">
       <div class="row">
-         <div class="col-xs-2">
-            <div class="dropdown">
-               <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                  <i class="fa fa-bars"></i>
-               </button>
-               <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                  <li><a href="#sec-title">Hero</a></li>
-                  <li><a href="#sec-manifesto">Manifesto</a></li>
-                  <li><a href="#sec-we-are">We Are</a></li>
-                  <li><a href="#sec-story">Our Story</a></li>
-                  <li><a href="#sec-action">Take Action</a></li>
-                  <li><a href="#sec-faqs">FAQs</a></li>
-                  <li><a href="#sec-footer">Get Some Newsletter</a></li>
-               </ul>
-            </div>
-         </div>
-         <div class="col-xs-8 text-center">
-            <img src="img/logo.png" height="75px">
-         </div>
          <div class="col-xs-12">
             <h2>Be a</h2>
             <h1>PROTOTYPE.</h1>
