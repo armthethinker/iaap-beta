f192533e23097fc9b436e6ef8b2b95390b248cdd
Andrew R McHugh <arm5629@gmail.com>
2015-06-28 11:19:41 -0400

    Added content and minor style update

--- includes/sections/we-are.php
+++ includes/sections/we-are.php
@@ -1,8 +1,10 @@
 <section class="section-we-are" id="section-we-are" style="background-image: url('/img/paper-bg.jpg')">
    <div class="section-overlay">
       <div class="row">
-         <div class="col-xs-12">
-            <h1>We are</h1>
+         <div class="col-lg-8 col-md-10 col-xs-12">
+            <h1>We're collecting stories and banding together</h1>
+            <p class="lead">For too long have we let others dictate how our lives "should" look.</p>
+            <p>We're coming out and letting people know our paths, our trials, our successes, and importantly our failures. Our lives don't fit into neat boxes, so let's stop thinking they should. Embrace being a prototype.</p>
          </div>
       </div>
    </div>
