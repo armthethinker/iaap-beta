c2866213ea5819a43d894076858e5e8d491ed6a6
Andrew R McHugh <arm5629@gmail.com>
2015-06-29 12:43:14 -0400

    Reorganized sections and removed we-are

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 <?php include('includes/sections/hero.php') ?>
 <?php include('includes/sections/manifesto.php') ?>
-<?php include('includes/sections/we-are.php') ?>
-<?php include('includes/sections/story.php') ?>
+<?php //include('includes/sections/we-are.php') ?>
 <?php include('includes/sections/action.php') ?>
+<?php include('includes/sections/story.php') ?>
 <?php include('includes/sections/faqs.php') ?>
 <?php include('includes/sections/footer.php') ?>
 
