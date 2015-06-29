e6f9b85d43b83a49923df93585f61cfc26b09c52
Andrew R McHugh <arm5629@gmail.com>
2015-06-29 12:42:29 -0400

    Added collapsable section

--- includes/sections/story.php
+++ includes/sections/story.php
@@ -3,45 +3,49 @@
       <div class="col-xs-12">
          <button class="pull-right btn btn-default btn-collapse-section" data-toggle="collapse" data-target=".story">Show</button>
          <h1>Our Story</h1>
-         <h2><small>Three Living Prototypes</small></h2>
-      </div>
-      <div class="col-lg-6">
-         <p>We'ver been there. Hell, we still are there. All three of us are trying to figure out our lives, but while we do it, we want to help others in their journyes too. We don't live fance lives, at least not from an outsider's perspective. But, we do have some damn fine stories.</p>
-         <p>Sarah and Andrew ran into each other by chance. Their conversation fueled Andrew's overnight in an airport, which generated the first semblance of I Am A Prototype.</p>
-         <p>Many conversations and months later, a founder lost and a founder gained, the three of us launched this project. We believe in our work's ability to inspire and bring people together.</p>
-      </div>
-      <div class="col-lg-6">
-         <div class="media profile-block">
-            <div class="media-left">
-               <a href="http://twitter.com/armthethinker">
-                  <img src="img/prof-andrew.png">
-               </a>
-            </div>
-            <div class="media-body">
-               <h4 class="media-title">Andrew R McHugh</h4>
-               <p class="small">Started some things. Now I'm learning from some of the best. Virtual reality is next. #iamaprototype</p>
-            </div>
-         </div>
-         <div class="media profile-block">
-            <div class="media-left">
-               <a href="http://twitter.com/armthethinker">
-                  <img src="img/prof-sarah.png">
-               </a>
-            </div>
-            <div class="media-body">
-               <h4 class="media-title">Sarah Kurland</h4>
-               <p class="small">I did this. I did that. Now I do the do.</p>
-            </div>
-         </div>
-         <div class="media profile-block">
-            <div class="media-left">
-               <a href="http://twitter.com/armthethinker">
-                  <img src="img/prof-abby.png">
-               </a>
-            </div>
-            <div class="media-body">
-               <h4 class="media-title">Abby Stokes</h4>
-               <p class="small">School: religious studies. Now: cooking veggie buddies. Organic farm: where I'm going, saving worlds with compost and sowing. #iamaprototype</p>
+         <div class="story collapse">
+            <h2><small>Three Living Prototypes</small></h2>
+            <div class="row">
+               <div class="col-lg-6">
+                  <p>We'ver been there. Hell, we still are there. All three of us are trying to figure out our lives, but while we do it, we want to help others in their journyes too. We don't live fance lives, at least not from an outsider's perspective. But, we do have some damn fine stories.</p>
+                  <p>Sarah and Andrew ran into each other by chance. Their conversation fueled Andrew's overnight in an airport, which generated the first semblance of I Am A Prototype.</p>
+                  <p>Many conversations and months later, a founder lost and a founder gained, the three of us launched this project. We believe in our work's ability to inspire and bring people together.</p>
+               </div>
+               <div class="col-lg-6">
+                  <div class="media profile-block">
+                     <div class="media-left">
+                        <a href="http://twitter.com/armthethinker">
+                           <img src="img/prof-andrew.png">
+                        </a>
+                     </div>
+                     <div class="media-body">
+                        <h4 class="media-title">Andrew R McHugh</h4>
+                        <p class="small">Started some things. Now I'm learning from some of the best. Virtual reality is next. #iamaprototype</p>
+                     </div>
+                  </div>
+                  <div class="media profile-block">
+                     <div class="media-left">
+                        <a href="http://twitter.com/armthethinker">
+                           <img src="img/prof-sarah.png">
+                        </a>
+                     </div>
+                     <div class="media-body">
+                        <h4 class="media-title">Sarah Kurland</h4>
+                        <p class="small">I did this. I did that. Now I do the do.</p>
+                     </div>
+                  </div>
+                  <div class="media profile-block">
+                     <div class="media-left">
+                        <a href="http://twitter.com/armthethinker">
+                           <img src="img/prof-abby.png">
+                        </a>
+                     </div>
+                     <div class="media-body">
+                        <h4 class="media-title">Abby Stokes</h4>
+                        <p class="small">School: religious studies. Now: cooking veggie buddies. Organic farm: where I'm going, saving worlds with compost and sowing. #iamaprototype</p>
+                     </div>
+                  </div>
+               </div>
             </div>
          </div>
       </div>
