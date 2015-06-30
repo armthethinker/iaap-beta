a96151dd1647a75e25e184e1db164684980f655e
Andrew R McHugh <arm5629@gmail.com>
2015-06-30 19:44:38 -0400

    Cleaned line endings

--- Gruntfile.js
+++ Gruntfile.js
@@ -248,12 +248,12 @@ module.exports = function(grunt) {
    });
 
    grunt.loadNpmTasks('grunt-contrib-uglify');
-   grunt.loadNpmTasks('grunt-contrib-less');    
+   grunt.loadNpmTasks('grunt-contrib-less'); 
    grunt.loadNpmTasks('grunt-contrib-watch');
-   grunt.loadNpmTasks('grunt-contrib-cssmin');   
-   grunt.loadNpmTasks('grunt-contrib-concat');   
-   grunt.loadNpmTasks('grunt-contrib-clean');    
-   grunt.loadNpmTasks('grunt-contrib-copy');    
+   grunt.loadNpmTasks('grunt-contrib-cssmin');
+   grunt.loadNpmTasks('grunt-contrib-concat');
+   grunt.loadNpmTasks('grunt-contrib-clean');
+   grunt.loadNpmTasks('grunt-contrib-copy');
    grunt.loadNpmTasks('grunt-ftp-deploy');
    grunt.loadNpmTasks('grunt-autoprefixer');
    grunt.loadNpmTasks('grunt-text-replace');
