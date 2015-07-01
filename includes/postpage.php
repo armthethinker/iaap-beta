704c2a9ac64eb9f9f0b247628fb691c12b71ad98
Andrew R McHugh <arm5629@gmail.com>
2015-06-30 20:01:40 -0400

    Added and disabled Google analytics

--- includes/postpage.php
+++ includes/postpage.php
@@ -3,5 +3,6 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
       <!-- Include all compiled plugins (below), or include individual files as needed -->
       <script src="dist/js/iamaprototype.min.js"></script>
+      <?php //include('includes/components/google-analytics.php') ?>
    </body>
 </html>
