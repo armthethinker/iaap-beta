cd435a4d0d0809cf0767dc4f0276ed60703270e7
Andrew R McHugh <arm5629@gmail.com>
2015-07-01 17:19:31 -0400

    Cleaned up formatting

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
-<?php include('includes/prepage.php') ?>
+<?php
 
-<?php include('includes/sections/hero.php') ?>
-<?php include('includes/sections/manifesto.php') ?>
-<?php //include('includes/sections/we-are.php') ?>
-<?php include('includes/sections/action.php') ?>
-<?php include('includes/sections/story.php') ?>
-<?php include('includes/sections/faqs.php') ?>
-<?php include('includes/sections/footer.php') ?>
+include('includes/prepage.php');
 
-<?php include('includes/components/rocket.php') ?>
+include('includes/sections/hero.php');
+include('includes/sections/manifesto.php');
+//include('includes/sections/we-are.php');
+include('includes/sections/action.php');
+include('includes/sections/story.php');
+include('includes/sections/faqs.php');
+include('includes/sections/footer.php');
 
-<?php include('includes/sections/m-prototype-fund.php') ?>
+include('includes/components/rocket.php');
 
-<?php include('includes/postpage.php') ?>
+include('includes/sections/m-prototype-fund.php');
+
+include('includes/postpage.php');
+
+?>
