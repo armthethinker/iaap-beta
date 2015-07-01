e05ae9346713720b849a4d8794b449aa771ed5f2
Andrew R McHugh <arm5629@gmail.com>
2015-06-30 22:17:29 -0400

    Revert "Got rid of extraneous font file: Droid Serif italic"
    This reverts commit 40f800ff05c23d9deab6c48c6c12c55d413ad087.

--- includes/prepage.php
+++ includes/prepage.php
@@ -8,8 +8,8 @@
    <title>I Am A Prototype - Being Change</title>
 
    <!-- Stylez -->
+   <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,400italic|Montserrat' rel='stylesheet' type='text/css'>
    <link href="dist/css/iamaprototype.min.css" rel="stylesheet">
-   <link href='http://fonts.googleapis.com/css?family=Droid+Serif|Montserrat' rel='stylesheet' type='text/css'>
    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
    <!--[if lt IE 9]>
