4dcd13945b56ea3ee7d8b5c9f67d638bc8d84c56
Andrew R McHugh <arm5629@gmail.com>
2015-07-04 11:20:12 -0400

    Added conditional manifesto template
    It shows a video if we have one, or if we’re developing for one.
    Otherwise, it shows the final product.

--- includes/sections/manifesto.php
+++ includes/sections/manifesto.php
@@ -13,13 +13,28 @@
                   <span class="badge">v1</span>
                </div>
             </div>
-            <div class="embed-responsive embed-responsive-16by9">
-               <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/8rwsuXHA7RA?rel=0" allowfullscreen></iframe>
-            </div>
-            <button class="btn btn-default center-block" type="button" data-toggle="collapse" data-target=".manifesto-text" aria-expanded="false" aria-controls="manifestoText">Read the Manifesto</button>
-            <div class="manifesto-text collapse p-t-sm">
-               <?php include('includes/md/MANIFESTO.html') ?>
-            </div>
+            <?php
+               $hasVideo = false;
+
+               if($hasVideo == true){
+                  ?>
+                     <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/8rwsuXHA7RA?rel=0" allowfullscreen></iframe>
+                     </div>
+                     <button class="btn btn-default center-block" type="button" data-toggle="collapse" data-target=".manifesto-text" aria-expanded="false" aria-controls="manifestoText">Read the Manifesto</button>
+                     <div class="manifesto-text collapse p-t-sm">
+                        <?php include('includes/md/MANIFESTO.html') ?>
+                     </div>
+                  <?php
+               }
+               else{
+                  ?>
+                     <div class="manifesto-text p-t-sm">
+                        <?php include('includes/md/MANIFESTO.html') ?>
+                     </div>
+                  <?php
+               }
+            ?>
          </div>
       </div>
    </div>
