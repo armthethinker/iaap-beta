e12cb8500099f1d5b71ba38209908c9a8c9d4142
Andrew R McHugh <arm5629@gmail.com>
2015-07-04 10:55:59 -0400

    Added Mailchimp signup form

---
+++ includes/components/mailchimp-signup.php
@@ -0,0 +1,29 @@
+<!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+   <form action="//iamaprototy.us10.list-manage.com/subscribe/post?u=8e4bbdc34154a011b79f2e4f1&amp;id=d25b2e4e07" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <div id="mc_embed_signup_scroll">
+
+         <div class="mc-field-group">
+            <div class="form-group">
+               <span class="hidden">
+                  <label for="mce-EMAIL">Email Address</label>
+               </span>
+               <div class="input-group">
+                  <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="Email address">
+                  <span class="input-group-btn">
+                     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                     <div style="position: absolute; left: -5000px;"><input type="text" name="b_8e4bbdc34154a011b79f2e4f1_d25b2e4e07" tabindex="-1" value=""></div>
+                     <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-primary ">
+                  </span>
+               </div>
+            </div>
+         </div>
+         <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+         </div>
+         
+      </div>
+   </form>
+</div>
+<!--End mc_embed_signup-->
--- includes/sections/footer.php
+++ includes/sections/footer.php
@@ -2,14 +2,7 @@
    <div class="row">
       <div class="col-sm-8">
          <h5>Sign up for snippets of awesome, not spam</h5>
-         <div class="form-group">
-            <div class="input-group">
-               <input type="email" class="form-control" placeholder="Email address">
-               <span class="input-group-btn">
-                  <button class="btn btn-primary" type="button">Sign Up</button>
-               </span>
-            </div>
-         </div>
+         <?php include('includes/components/mailchimp-signup.php') ?>
          <p class="small">You’ll be the first to know about our stories, new products, and updates as the movement grows. Plus, we’ll give you 5% off of a purchase!</p>
       </div>
       <div class="col-sm-4">
