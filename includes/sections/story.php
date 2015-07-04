9fbd5eac93913655225cb1f35c18394603635f32
Andrew R McHugh <arm5629@gmail.com>
2015-07-03 21:46:01 -0400

    Better responsive breakpoint

--- includes/sections/story.php
+++ includes/sections/story.php
@@ -6,12 +6,12 @@
          <div class="story collapse">
             <h2><small>Three Living Prototypes</small></h2>
             <div class="row">
-               <div class="col-lg-6">
+               <div class="col-sm-6">
                   <p>We'ver been there. Hell, we still are there. All three of us are trying to figure out our lives, but while we do it, we want to help others in their journyes too. We don't live fance lives, at least not from an outsider's perspective. But, we do have some damn fine stories.</p>
                   <p>Sarah and Andrew ran into each other by chance. Their conversation fueled Andrew's overnight in an airport, which generated the first semblance of I Am A Prototype.</p>
                   <p>Many conversations and months later, a founder lost and a founder gained, the three of us launched this project. We believe in our work's ability to inspire and bring people together.</p>
                </div>
-               <div class="col-lg-6">
+               <div class="col-sm-6">
                   <div class="media profile-block">
                      <div class="media-left">
                         <a href="http://twitter.com/armthethinker">
