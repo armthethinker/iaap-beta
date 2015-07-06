0c7228001c5fff37f42db9d446decd4e267a3f10
Andrew R McHugh <arm5629@gmail.com>
2015-07-06 19:54:49 -0400

    Added just for feedback rounds

--- includes/components/nav.php
+++ includes/components/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-affix">
    <div class="container-fluid">
       <div class="row">
-         <div class="col-xs-2">
+         <div class="col-xs-4">
             <div class="dropdown">
                <button class="btn btn-default btn-outline btn-navbar-menu dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                   <span class="icon-bar"></span>
@@ -20,8 +20,11 @@
                </ul>
             </div>
          </div>
-         <div class="col-xs-8 text-center">
+         <div class="col-xs-4 text-center">
             <img src="img/logo.png" class="img-navbar-brand">
+         </div>
+         <div class="col-xs-4 text-right">
+            <a class="nav-link" href="http://goo.gl/forms/apaGzwW7hw" target="_blank"><i class="fa fa-check-circle-o"></i> Feedback<span class="hidden-xs"> Form</span></a>
          </div>
       </div>
    </div>
