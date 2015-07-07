084065dba84bffd0732efd229209b6afe95083be
Andrew R McHugh <arm5629@gmail.com>
2015-07-07 13:00:43 -0400

    Capitalized PROTOTYPE

--- includes/sections/m-prototype-fund.php
+++ includes/sections/m-prototype-fund.php
@@ -4,11 +4,11 @@
       <div class="modal-content">
          <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="m-prototype-fund-label">What is the <span class="text-primary">Prototype Fund</span>?</h4>
+            <h4 class="modal-title" id="m-prototype-fund-label">What is the <span class="text-primary">PROTOTYPE Fund</span>?</h4>
          </div>
          <div class="modal-body">
-            <p class="lead">10% of the profit on each purchase goes into a revolving fund. In disbursments of $1,000, fellow prototypes will get funds to push their projects forward.</p>
-            <p>When we started this, we knew that we wanted to give back. Each of us have struggled to fund our ideas, or even just to keep the lights on. That’s why 10% of each purchase is put into a revolving fund. In disbursments of $1,000, we’ll give prototypes funding and a bit of mentorship. Applications will open once we’ve built up enough of a fund.</p>
+            <p class="lead">10% of the profit on each purchase goes into a revolving fund. In disbursments of $1,000, fellow PROTOTYPES will get funds to push their projects forward.</p>
+            <p>When we started this, we knew that we wanted to give back. Each of us have struggled to fund our ideas, or even just to keep the lights on. That’s why 10% of each purchase is put into a revolving fund. In disbursments of $1,000, we’ll give PROTOTYPES funding and a bit of mentorship. Applications will open once we’ve built up enough of a fund.</p>
             <p>If you are interested, you should sign up for our newsletter. We'll let you know when the program opens.</p>
          </div>
          <div class="modal-footer">
