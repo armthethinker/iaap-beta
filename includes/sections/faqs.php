0f4b0537e5be1587a70e8a3cbfb6288cf4218965
Andrew R McHugh <arm5629@gmail.com>
2015-07-07 13:00:29 -0400

    Restructured and hid two FAQs

--- includes/sections/faqs.php
+++ includes/sections/faqs.php
@@ -16,10 +16,6 @@
                         <p>We’re PROTOTYPES in every which way, including our company. We’ll always be testing new things. Right now, we’re focused on launching and connecting this community. Soon, we’ll show you a few things we have up our sleeves.</p>
                         <p>Have an idea? Shoot us an <a href="#">email</a>.</p>
                      </div>
-                     <div class="faq">
-                        <h4>What about stories longer than a tweet?</h4>
-                        <p>We love those too! We’re working on a Medium publication for just that. If you want to submit a story, or would like one of us to interview you and write your story, shoot us an <a href="#">email</a>.</p>
-                     </div>
                   </div>
                   <div class="col-sm-6">
                      <div class="faq">
@@ -27,13 +23,20 @@
                         <p>Every purchase directly affects a fellow PROTOTYPE. A good chunk of your purchase goes towards printing and shipping your item. The profit is split between the founders and 10% of that profit goes into the <a href="#" data-toggle="modal" data-target="#m-prototype-fund">PROTOTYPE Fund</a>.</p>
                      </div>
                      <div class="faq">
-                        <h4>Prototype Fund?</h4>
-                        <p>When we started this, we knew that we wanted to give back. Each of us have struggled to fund our ideas, or even just to keep the lights on. That’s why 10% of each purchase is put into a revolving fund. In disbursments of $1,000, we’ll give prototypes funding and a bit of mentorship. Applications will open once we’ve built up enough of a fund.</p>
+                        <h4>What about stories longer than a tweet?</h4>
+                        <p>We love those too! We’re working on a Medium publication for just that. If you want to submit a story, or would like one of us to interview you and write your story, shoot us an <a href="#">email</a>.</p>
                      </div>
-                     <div class="faq">
-                        <h4>I got charged multiple times for shipping.</h4>
-                        <p>That isn't a question, but we understand what you mean. Because of how our operations are curently setup, we don’t make your order until you buy it. The upside is our products are always available. The downside is that sometimes you get charged twice when purchasing different items together.</p>
-                     </div>
+                     <?php
+                     // <div class="faq">
+                     //    <h4>PROTOTYPE Fund?</h4>
+                     //    <p>When we started this, we knew that we wanted to give back. Each of us have struggled to fund our ideas, or even just to keep the lights on. That’s why 10% of each purchase is put into a revolving fund. In disbursments of $1,000, we’ll give PROTOTYPES funding and a bit of mentorship. Applications will open once we’ve built up enough of a fund.</p>
+                     // </div>
+                     
+                     // <div class="faq">
+                     //    <h4>I got charged multiple times for shipping.</h4>
+                     //    <p>That isn't a question, but we understand what you mean. Because of how our operations are curently setup, we don’t make your order until you buy it. The upside is our products are always available. The downside is that sometimes you get charged twice when purchasing different items together.</p>
+                     // </div>
+                     ?>
                   </div>
                </div>
             </div>
