dc5ef35ad9f331260b8a6147de2ea8dd3ce8ad16
Andrew R McHugh <arm5629@gmail.com>
2015-07-07 13:01:35 -0400

    Embedded Twitter collection

--- includes/sections/photo2.php
+++ includes/sections/photo2.php
@@ -1,3 +1,9 @@
 <section class="section-photo photo2" style="background-image: url('img/photoshoots/IAAP-36.jpg')">
-   <div class="section-overlay"></div>
+   <div class="section-overlay">
+      <div class="row">
+         <div class="col-md-6 col-md-offset-3">
+            <?php include('includes/components/twitter-embed.php') ?>
+         </div>
+      </div>
+   </div>
 </section>
